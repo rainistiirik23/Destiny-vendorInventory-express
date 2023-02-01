637ae8c933b8e0b2ae6940642eac1d42b8e025b1
rainistiirik23 <rainis.tiirik9@gmail.com>
2023-02-01 14:39:50 +0200

    Added https  for recieving authorization code

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const https = require('https');
 const banshee = require('./Routes/banshee');
 const Vendorpage = require('./Routes/Vendorpage');
 const wishList = require('./Routes/wishList');
 const gunPic = require('./Routes/gunPic');
 const gunPicView = require('./Routes/gunPicView');
+const fs = require('fs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/gunPic', gunPic);
@@ -13,7 +15,14 @@ app.use('/gunPicView', gunPicView);
 app.use('/', Vendorpage);
 app.use('/banshee', banshee);
 app.use('/wishlist', wishList);
-
-app.listen(port = 8000, () => {
+const options = {
+    cert: fs.readFileSync("././ssl/cert.pem"),
+    key: fs.readFileSync("././ssl/key.pem")
+};
+const server = https.createServer(options, app)
+server.listen(port = 8000, () => {
+    console.log(`Server now listening at https://localhost:${port}`);
+})
+/* app.listen(port = 8000, () => {
     console.log(`Server now listening at http://localhost:${port}`);
-});
+}); */
