0e994b9a2affcbcaae9f97497aff11afb40fc8c2
rainistiirik23 <rainis.tiirik9@gmail.com>
2023-02-03 14:51:46 +0200

    New path for screenshot.

--- Server/Controllers/gunPic.js
+++ Server/Controllers/gunPic.js
@@ -4,11 +4,11 @@ const app = express()
 const fs = require('fs/promises')
 const puppeteer = require('puppeteer');
 
-const screenShot = async () => {
-    const url = 'http://localhost:8000/gunPicView';
+/* const screenShot = async () => {
+    const url = 'https://localhost:8000/gunPicView';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, /* { 'waitUntil': 'networkidle2' } */);
+    await page.goto(url);
 
     await page.screenshot({
         path: path.join(__dirname, '../../Pictures'),
@@ -21,10 +21,10 @@ const screenShot = async () => {
 
     });
     await browser.close();
-}
+} */
 
 const gunPic = async (req, res) => {
-    const dirRead = await fs.readdir(path.join(__dirname, '../../Pictures'), (err, result) => {
+    const dirRead = await fs.readdir(path.join(__dirname, '../Assets/Pictures/Banshee'), (err, result) => {
         if (err) {
             console.log(err)
         }
@@ -32,13 +32,15 @@ const gunPic = async (req, res) => {
 
     if (dirRead.length == 0) {
 
-        const url = 'http://localhost:8000/gunPicView';
-        const browser = await puppeteer.launch();
+        const url = 'https://localhost:8000/gunPicView';
+        const browser = await puppeteer.launch({
+            ignoreHTTPSErrors: true
+        });
         const page = await browser.newPage();
         await page.goto(url);
         setTimeout(async () => {
             await page.screenshot({
-                path: path.join(__dirname, '../../Pictures/screenshot.png'),
+                path: path.join(__dirname, '../Assets/Pictures/Banshee/screenshot.png'),
                 /* clip: {
                     x: 3,
                     y: 0,
@@ -49,12 +51,12 @@ const gunPic = async (req, res) => {
 
             });
             await browser.close();
-            await res.sendFile(path.join(__dirname, '../../Pictures/screenshot.png'))
+            await res.sendFile(path.join(__dirname, '../Assets/Pictures/Banshee/screenshot.png'))
 
         }, 3000)
         return;
     }
-    res.sendFile(path.join(__dirname, '../../Pictures/screenshot.png'))
+    res.sendFile(path.join(__dirname, '../Assets/Pictures/Banshee/screenshot.png'))
 };
 
 module.exports = gunPic
