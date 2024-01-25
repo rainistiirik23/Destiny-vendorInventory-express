18d4234d537cf4a291a4d53cce0b6125b12e8208
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-01-25 13:28:54 +0200

    Adjusted config properties and removed credentials variable + file format

--- Requests/TokenRefreshRequest.js
+++ Requests/TokenRefreshRequest.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
 const request = require("request");
-const credentials = require('../Config/config.json')
-const { ApiKey, client_id, client_secret, refresh_token } = credentials
+const {
+  Api: { ApiKey, client_id, client_secret, refresh_token },
+} = require("../Config/config.json");
 const X = btoa(client_id + ":" + client_secret);
 
 /* While it's possible to log in again and again to bungie.net with the account to get the access token and make a VendorRequest,
@@ -10,70 +11,71 @@ NB!! refresh token has an expiration date of 90 days,after that you will  have t
 */
 
 const tokenRequest = () => {
-    return (
-        new Promise((resolve, reject) => {
-            request.post({
-                url: 'https://www.bungie.net/Platform/App/OAuth/Token/',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'X-API-Key': ApiKey,
-                    'Authorization': 'Basic ' + X,
-                },
+  return new Promise((resolve, reject) => {
+    request.post(
+      {
+        url: "https://www.bungie.net/Platform/App/OAuth/Token/",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "X-API-Key": ApiKey,
+          Authorization: "Basic " + X,
+        },
 
-                form: {
-                    "grant_type": "refresh_token",
-                    "refresh_token": refresh_token,
-                }
-
-            }, (err, res, body) => {
-                const moreErrorInfo = body + " \nstatus code: " + res.statusCode;
-                const bodyAsJSon = JSON.parse(body);
-                if (bodyAsJSon.error) {
-                    reject(moreErrorInfo);
-                }
-                else {
-                    resolve(bodyAsJSon);
-                }
-            });
-        }));
+        form: {
+          grant_type: "refresh_token",
+          refresh_token: refresh_token,
+        },
+      },
+      (err, res, body) => {
+        const moreErrorInfo = body + " \nstatus code: " + res.statusCode;
+        const bodyAsJSon = JSON.parse(body);
+        if (bodyAsJSon.error) {
+          reject(moreErrorInfo);
+        } else {
+          resolve(bodyAsJSon);
+        }
+      }
+    );
+  });
 };
 
 const readTokenConfig = () => {
-    return (
-        new Promise((resolve, reject) => {
-            fs.readFile('Config/config.json', 'utf8', (err, res) => {
-                const resultAsJson = JSON.parse(res)
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve(resultAsJson)
-                }
-            });
-        }));
+  return new Promise((resolve, reject) => {
+    fs.readFile("Config/config.json", "utf8", (err, res) => {
+      const resultAsJson = JSON.parse(res);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(resultAsJson);
+      }
+    });
+  });
 };
 
-const writeTokens = (configTokens, requestTokens) => new Promise((resolve, reject) => {
-    configTokens['access_token'] = requestTokens['access_token'];
-    configTokens[' refresh_token'] = requestTokens[' refresh_token'];
-    fs.writeFile('Config/config.json', JSON.stringify(configTokens), (err, result) => {
+const writeTokens = (configTokens, requestTokens) =>
+  new Promise((resolve, reject) => {
+    configTokens["access_token"] = requestTokens["access_token"];
+    configTokens[" refresh_token"] = requestTokens[" refresh_token"];
+    fs.writeFile(
+      "Config/config.json",
+      JSON.stringify(configTokens),
+      (err, result) => {
         if (err) {
-            reject(err);
+          reject(err);
+        } else {
+          resolve(console.log("Tokens are written"));
         }
-        else {
-            resolve(console.log('Tokens are written'));
-        }
-    })
-});
+      }
+    );
+  });
 
 const refreshToken = async () => {
-    try {
-        const requestTokens = await tokenRequest();
-        const configTokens = await readTokenConfig();
-        await writeTokens(configTokens, requestTokens);
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const requestTokens = await tokenRequest();
+    const configTokens = await readTokenConfig();
+    await writeTokens(configTokens, requestTokens);
+  } catch (err) {
+    console.log(err);
+  }
+};
 refreshToken();
