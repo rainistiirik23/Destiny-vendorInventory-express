1f66521ad38b4b2a68b8aae9284ad1b71c4e2f06
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-02 11:59:34 +0300

    Added more fields

--- Database/ItemManifestToDatabase.js
+++ Database/ItemManifestToDatabase.js
@@ -1,102 +1,139 @@
-const mysql = require('mysql');
-const sqlite3 = require('sqlite3');
-const { Database:
-    {
-        host: host,
-        user: databaseUser,
-        password: password,
-        database: dataBaseName
-    }
-} = require('../Config/config.json');
+const mysql = require("mysql");
+const sqlite3 = require("sqlite3");
+const fs = require("fs");
+const {
+  Database: { host: host, user: databaseUser, password: password, dataBaseName: dataBaseName },
+} = require("../Config/config.json");
 
 const getItemManifest = async () => {
-    let db = new sqlite3.Database('dist/world_sql_content_50f67b17bc243f7570787a58395230db.content', sqlite3.OPEN_READONLY, error => {
-        if (error) {
-            console.log(error)
-        }
+  let db = new sqlite3.Database(
+    "dist/world_sql_content_14e4321b227a904753480705bc9a3651.content",
+    sqlite3.OPEN_READONLY,
+    (error) => {
+      if (error) {
+        /*  console.log(error); */
+      }
+    }
+  );
+  const itemManifest = await new Promise((resolve, reject) => {
+    db.all("Select json from DestinyInventoryItemDefinition", (error, result) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
     });
-    const itemManifest = await new Promise((resolve, reject) => {
-        db.all('Select json from DestinyInventoryItemDefinition', (error, result) => {
+    /*  console.log(manifest); */
+  });
+
+  return itemManifest;
+};
+const doesItemManifestTableExist = (manifestDescription, mysqlConnection) => {
+  return new Promise((resolve, reject) => {
+    mysqlConnection.query(manifestDescription, (error, result) => {
+      if (error) {
+        if (error.errno === 1146) {
+          console.log(error);
+          const ItemManifestTable = `CREATE TABLE Item_manifest (
+                              id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                              itemName VARCHAR(255) NOT NULL,
+                              itemDescription TEXT,
+                              itemIcon  TEXT,
+                              itemHash BIGINT(255) NOT NULL,
+                              itemTypeDisplayName TEXT,
+                              itemFlavorText TEXT,
+                              itemTypeAndTierDisplayName VARCHAR(255),
+                              plug_category_identifier VARCHAR(255)
+                              );`;
+
+          mysqlConnection.query(ItemManifestTable, (error, result) => {
             if (error) {
-                reject(error);
+              console.log(error);
+              reject(error);
+            } else {
+              console.log(result);
+              resolve();
             }
-            resolve(result);
-        });
-        /*  console.log(manifest); */
-    })
-
-    return itemManifest;
+          });
+        } else if (error.errno !== 1146) {
+          console.log(error);
+          reject(error);
+        }
+      } else {
+        console.log("Table found");
+        resolve();
+      }
+    });
+  });
 };
-
 const insertData = async (mysqlConnection, manifest) => {
-
+  return new Promise((resolve, reject) => {
     for (let i = 0; i < manifest.length; i++) {
-
-        const manifestAsJson = JSON.parse(manifest[i]['json']);
-        const {
-            displayProperties: { name: itemName, icon: itemIcon, },
-            hash: itemHash,
-            itemTypeDisplayName: itemTypeDisplayName,
-            flavorText: itemFlavorText,
-            itemTypeAndTierDisplayName: itemTypeAndTierDisplayName,
-        } = manifestAsJson;
-        const insertManifest = `INSERT INTO Item_manifest (itemName,itemIcon,itemHash,itemTypeDisplayName,itemFlavorText,itemTypeAndTierDisplayName) VALUES(?, ?, ?, ?, ?, ?);`;
-        mysqlConnection.query(insertManifest,
-            [
-                itemName, itemIcon,
-                itemHash, itemTypeDisplayName,
-                itemFlavorText, itemTypeAndTierDisplayName
-
-            ], (error, result) => {
-                if (error) {
-                    throw error;
-                }
-            });
+      const manifestAsJson = JSON.parse(manifest[i]["json"]);
+      /*  if (manifestAsJson.hash === 3511092054) {
+        console.log(manifestAsJson);
+      } */
+      const {
+        displayProperties: { name: itemName, description, icon: itemIcon },
+        hash: itemHash,
+        itemTypeDisplayName: itemTypeDisplayName,
+        flavorText: itemFlavorText,
+        itemTypeAndTierDisplayName: itemTypeAndTierDisplayName,
+        plug: { plugCategoryIdentifier = null } = {},
+      } = manifestAsJson;
+      const insertManifest = `INSERT INTO Item_manifest (itemName,itemDescription,itemIcon,itemHash,itemTypeDisplayName,itemFlavorText,itemTypeAndTierDisplayName,plug_category_identifier) VALUES(?, ?, ?, ?, ?, ?, ?,?);`;
+      mysqlConnection.query(
+        insertManifest,
+        [
+          itemName,
+          description,
+          itemIcon,
+          itemHash,
+          itemTypeDisplayName,
+          itemFlavorText,
+          itemTypeAndTierDisplayName,
+          plugCategoryIdentifier,
+        ],
+        (error, result) => {
+          /*  if (manifestAsJson.hash === 3511092054) {
+            console.log(error);
+          } */
+          if (error) {
+            reject(error);
+          }
+        }
+      );
     }
+    resolve();
+  });
 };
-
+const endMysqlConnection = (mysqlConnection) => {
+  return new Promise((resolve, reject) => {
+    mysqlConnection.end((error) => {
+      if (error) {
+        reject(error);
+      }
+      console.log("closed mysql connection");
+      resolve();
+    });
+  });
+};
 const manifestToDatabase = async () => {
-    try {
-        const manifest = await getItemManifest();
+  try {
+    const manifest = await getItemManifest();
 
-        const mysqlConnection = mysql.createConnection({
-            host: host,
-            user: databaseUser,
-            password: password,
-            database: dataBaseName
-        });
-        const describeManifest = `DESCRIBE Item_manifest;`;
-        mysqlConnection.query(describeManifest, (error, result) => {
-            if (error && error.errno === 1146) {
-                console.log(error);
-                const ItemManifestTable = `CREATE TABLE Item_manifest (
-                        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-                        itemName VARCHAR(255) NOT NULL,
-                        itemIcon  TEXT,
-                        itemHash BIGINT(255) NOT NULL,
-                        itemTypeDisplayName VARCHAR(255) ,
-                        itemFlavorText TEXT,
-                        itemTypeAndTierDisplayName VARCHAR(255)
-                        );`;
+    const mysqlConnection = mysql.createConnection({
+      host: host,
+      user: databaseUser,
+      password: password,
+      database: dataBaseName,
+    });
 
-                mysqlConnection.query(ItemManifestTable, (error, result) => {
-                    if (error) {
-                        console.log(error);
-                    }
-                    else {
-                        console.log(result);
-                    }
-                })
-            }
-            else {
-                console.log("Table found");
-            }
-        })
-
-        await insertData(mysqlConnection, manifest);
-    }
-    catch (error) {
-        console.log(error)
-    }
+    const describeManifest = `DESCRIBE Item_manifest;`;
+    await doesItemManifestTableExist(describeManifest, mysqlConnection);
+    await insertData(mysqlConnection, manifest);
+    endMysqlConnection(mysqlConnection);
+  } catch (error) {
+    console.log(error);
+  }
 };
 manifestToDatabase();
