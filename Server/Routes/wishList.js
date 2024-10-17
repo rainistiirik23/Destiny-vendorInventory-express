ae19dcbb03a5e7368761a05fec5c7076dfdce2ba
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-10-17 17:43:23 +0300

    Made a wishlist route file for wishlist related routes

--- Server/Routes/wishList.js
+++ Server/Routes/wishList.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const RenderWishList = require('../Controllers/wishlist');
-
-router.use('/', express.static(path.join(__dirname, '../../', 'Public/Views/Wishlistpage')));
-router.get('/', RenderWishList);
-
-module.exports = router
+const path = require("path");
+const RenderWishList = require("../Controllers/wishlist");
+const editWishlistedItem = require("../Controllers/WishList/editWishlistedItem");
+const deleteWishlistedItem = require("../Controllers/WishList/deleteWishlistedItem");
+const checkForWishListedItemFunction = require("../Controllers/WishList/checkForWishListedItem");
+const usersWishListedSales = require("../Controllers/WishList/getUsersWishListedSales");
+const saveWishLIstedItemController = require("../Controllers/WishList/saveWishListedItem");
+const corsOptions = {
+  origin: "http://127.0.0.1:5173",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+module.exports = function (router) {
+  router.put("/api/editWishListedItem", editWishlistedItem);
+  router.delete("/api/deleteWishlistedItem", deleteWishlistedItem);
+  router.post("/api/checkForWishListedItem", checkForWishListedItemFunction);
+  router.post("/api/getUsersWishListedSales", usersWishListedSales);
+  router.post("/api/saveWishListedItem", saveWishLIstedItemController);
+};
