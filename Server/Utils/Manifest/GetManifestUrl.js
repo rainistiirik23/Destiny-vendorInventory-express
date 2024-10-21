8ffd3aef83a80c16b0cb0f5c6ec2c61e92a2cb96
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-10-21 15:56:06 +0300

    Replaced request with axios

--- Server/Utils/Manifest/GetManifestUrl.js
+++ Server/Utils/Manifest/GetManifestUrl.js
@@ -1,42 +1,23 @@
 const {
-  Api: { ApiKey, client_id, client_secret, code },
-} = require("../../../Config/config.json");
-const request = require("request");
+  Api: { ApiKey, client_id, client_secret, code, manifestUrl: configManifestUrl },
+} = require("../../Config/config.json");
+
+const axios = require("axios");
 const fs = require("fs");
 const X = btoa(client_id + ":" + client_secret);
+const axiosRequestInstance = axios.create({
+  baseURL: "https://www.bungie.net/Platform/Destiny2/Manifest/",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "X-API-Key": ApiKey,
+    Authorization: "Basic " + X,
+    client_id: client_id,
+    code: code,
+  },
+});
 
-const manifestUrlRequest = () => {
-  return new Promise((resolve, reject) => {
-    request.get(
-      {
-        url: "https://www.bungie.net/Platform/Destiny2/Manifest/ ",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "X-API-Key": ApiKey,
-          Authorization: "Basic " + X,
-        },
-        form: {
-          /*  'grant_type': 'authorization_code', */
-          client_id: client_id,
-          code: code,
-        },
-      },
-      function (err, res, body) {
-        if (err) {
-          reject(err);
-        }
-        const jsonResponse = JSON.parse(body);
-        console.log(jsonResponse);
-        console.log(credentials.Api.manifestUrl);
-        credentials.Api.manifestUrl = `https://www.bungie.net${jsonResponse.Response.mobileWorldContentPaths.en}`;
-        resolve(jsonResponse.Response.mobileWorldContentPaths.en);
-      }
-    );
-  });
-};
 const saveManifestUrl = (manifestUrl) => {
   return new Promise((resolve, reject) => {
-    /* credentials.Api.manifestUrl = manifestUrl; */
     fs.writeFile("Config/config.json", JSON.stringify(credentials), (error, result) => {
       if (error) {
         reject(error);
@@ -45,11 +26,21 @@ const saveManifestUrl = (manifestUrl) => {
     });
   });
 };
-async function getManifestURl(overrideConfigManifestUrl) {
-  const manifestUrl = await manifestUrlRequest();
-  if (!overrideConfigManifestUrl) {
-    return;
+
+async function getManifestURl() {
+  try {
+    console.log("active");
+    const manifestUrlRequest = await axiosRequestInstance.get();
+    const manifestUrl = `https://www.bungie.net${manifestUrlRequest.data.Response.mobileWorldContentPaths.en}`;
+    console.log(manifestUrlRequest);
+
+    if (configManifestUrl === manifestUrl) {
+      return;
+    }
+    /*  await saveManifestUrl(manifestUrl); */
+  } catch (error) {
+    console.error(error);
   }
-  await saveManifestUrl(manifestUrl);
 }
-module.exports = getManifestURl;
+getManifestURl();
+/* module.exports = getManifestURl; */
