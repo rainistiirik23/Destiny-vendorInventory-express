51676adf6aa4db3d6dc2cbc2adf37290d63c157a
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-10-22 13:33:42 +0300

    Wrapped the request into a promise and added to module exports

--- Server/Utils/Manifest/GetManifestSqlFile.js
+++ Server/Utils/Manifest/GetManifestSqlFile.js
@@ -1,17 +1,28 @@
 const https = require("https");
 const {
   Api: { manifestUrl },
-} = require("../../../Config/config.json");
-
+} = require("../../Config/config.json");
 const fs = require("fs");
 
-https.get(manifestUrl, (res) => {
-  const path = "sqliteFile";
-  const writeStream = fs.createWriteStream(path);
-  res.pipe(writeStream);
+function getManifestSqlFile() {
+  return new Promise((resolve, reject) => {
+    const manifestRequest = https.get(manifestUrl);
+    manifestRequest.on("response", (response) => {
+      if (response.statusCode !== 200) {
+        reject(
+          `Retrieving manifest sql file failed with statuscode ${response.statusCode} and status message ${response.statusMessage}`
+        );
+        return;
+      }
+      const writeStream = fs.createWriteStream("Server/Storage/Manifest/SqlLite/manifestSqlLiteFile");
 
-  writeStream.on("finish", () => {
-    writeStream.close();
-    console.log("Download Completed");
+      response.pipe(writeStream);
+      writeStream.on("finish", () => {
+        writeStream.close();
+        console.log("Download Completed");
+        resolve();
+      });
+    });
   });
-});
+}
+module.exports = getManifestSqlFile;
