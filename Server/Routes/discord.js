04c13fd5e6bd4bbb1bde810266b0abf3529a62e5
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-10-28 16:30:10 +0200

    Added cors option

--- Server/Routes/discord.js
+++ Server/Routes/discord.js
@@ -1,4 +1,6 @@
+const cors = require("cors");
+const { corsOptions } = require("../Config/config.json");
 const getUserId = require("../Controllers/Discord/getUserId");
 module.exports = function (router) {
-  router.post("/api/getUserId", getUserId);
+  router.post("/api/getUserId", cors(corsOptions), getUserId);
 };
--- Server/Routes/wishList.js
+++ Server/Routes/wishList.js
@@ -1,20 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-const RenderWishList = require("../Controllers/wishlist");
+const cors = require("cors");
+const { corsOptions } = require("../Config/config.json");
 const editWishlistedItem = require("../Controllers/WishList/editWishlistedItem");
 const deleteWishlistedItem = require("../Controllers/WishList/deleteWishlistedItem");
 const checkForWishListedItemFunction = require("../Controllers/WishList/checkForWishListedItem");
 const usersWishListedSales = require("../Controllers/WishList/getUsersWishListedSales");
 const saveWishLIstedItemController = require("../Controllers/WishList/saveWishListedItem");
-const corsOptions = {
-  origin: "http://127.0.0.1:5173",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+
 module.exports = function (router) {
-  router.put("/api/editWishListedItem", editWishlistedItem);
-  router.delete("/api/deleteWishlistedItem", deleteWishlistedItem);
-  router.post("/api/checkForWishListedItem", checkForWishListedItemFunction);
-  router.post("/api/getUsersWishListedSales", usersWishListedSales);
-  router.post("/api/saveWishListedItem", saveWishLIstedItemController);
+  router.put("/api/editWishListedItem", cors(corsOptions), editWishlistedItem);
+  router.delete("/api/deleteWishlistedItem", cors(corsOptions), deleteWishlistedItem);
+  router.post("/api/checkForWishListedItem", cors(corsOptions), checkForWishListedItemFunction);
+  router.post("/api/getUsersWishListedSales", cors(corsOptions), usersWishListedSales);
+  router.post("/api/saveWishListedItem", cors(corsOptions), saveWishLIstedItemController);
 };
