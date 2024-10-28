a113d1793759c5f809cdac63b15b7f7f56ed8d2b
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-10-28 16:30:37 +0200

    Added correct path for vendor controller modules

--- Server/Routes/vendorSales.js
+++ Server/Routes/vendorSales.js
@@ -1,5 +1,5 @@
-const allVendorSales = require("../Controllers/getAllVendorSales");
-const currentVendorSales = require("../Controllers/getCurrentVendorSales");
+const allVendorSales = require("../Controllers/Vendor/getAllVendorSales");
+const currentVendorSales = require("../Controllers/Vendor/getCurrentVendorSales");
 
 module.exports = function (router) {
   router.get("/api/allVendorSales", allVendorSales);
