29f4aa38c4ec1d2134135cd8dc830c68f8175f01
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-10-28 15:50:08 +0200

    Removed commented properties

--- Server/Utils/Requests/TokenRequest.js
+++ Server/Utils/Requests/TokenRequest.js
@@ -6,23 +6,8 @@ const fs = require("fs");
 const X = Buffer.from(client_id + ":" + client_secret).toString("base64");
 const axios = require("axios");
 const axiosRequestInstance = axios.create({
-  /*  params: new URLSearchParams({
-    grant_type: "authorization_code",
-    code: code,
-    client_id: client_id,
-  }), */
-  /*  baseURL: `https://www.bungie.net/Platform/App/OAuth/Token/`,
-  data: qs.stringify({
-    code: code,
-    grant_type: "authorization_code",
-    client_id: client_id,
-  }),
- */
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
-    /*  "X-API-Key": ApiKey,
-    hasContentType: true,
-    Authorization: `Basic ${X}`, */
   },
 });
 // Request for a Token which Oauth requires to make a VendorRequest
