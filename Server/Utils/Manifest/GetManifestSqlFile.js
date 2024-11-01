c572b106fb5fb1735ab893b22c6ce28d7324b8df
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-11-01 12:07:40 +0200

    Moved require into promise(comment for clarity)

--- Server/Utils/Manifest/GetManifestSqlFile.js
+++ Server/Utils/Manifest/GetManifestSqlFile.js
@@ -1,11 +1,15 @@
 const https = require("https");
-const {
-  Api: { manifestUrl },
-} = require("../../Config/config.json");
 const fs = require("fs");
-
+/*
+This function needs the correct manifest url, which will sometimes need to be updated,
+because of that config require is in the promise code block due to it not reading the correct manifest version
+after node fs writefile function is called
+*/
 function getManifestSqlFile() {
   return new Promise((resolve, reject) => {
+    const {
+      Api: { manifestUrl },
+    } = require("../../Config/config.json");
     const manifestRequest = https.get(manifestUrl);
     manifestRequest.on("response", (response) => {
       if (response.statusCode !== 200) {
