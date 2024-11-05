80f0565eb3e3fede71e027e4b6217e49a14e1108
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-11-05 16:27:22 +0200

    Replaced request with axios,changed config path

--- Server/Utils/Requests/VendorRequest.js
+++ Server/Utils/Requests/VendorRequest.js
@@ -1,50 +1,40 @@
 const fs = require("fs");
-const request = require("request");
-const credentials = require("../../../Config/config.json");
+const axios = require("axios");
+
+const credentials = require("../../Config/config.json");
 const {
   Api: { ApiKey, access_token },
   SteamAccount: { memberShipType, memberShipId, characterId },
 } = credentials;
-
+const axiosRequestInstance = axios.create({
+  headers: {
+    "X-API-KEY": ApiKey,
+    Authorization: "Bearer " + access_token,
+  },
+});
 // Make a request for a vendor's inventory
-const vendorRequest = () => {
-  return new Promise((resolve, reject) => {
-    request.get(
-      {
-        url: `https://www.bungie.net/Platform/Destiny2/${memberShipType}/Profile/${memberShipId}/Character/${characterId}/Vendors/672118013/?components=402`,
-        headers: {
-          "X-API-KEY": ApiKey,
-          Authorization: "Bearer " + access_token,
-        },
-      },
-      (err, res, body) => {
-        console.log(body);
-        const bodyAsJson = JSON.parse(body);
 
-        if (bodyAsJson.error) {
-          const moreErrorInfo = body + " \nstatus code: " + res.statusCode;
-          reject(moreErrorInfo);
+const cacheVendorRequest = (vendorRequestResponse) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(
+      "./Cache/VendorRequest.json",
+      JSON.stringify(vendorRequestResponse),
+      { flag: "w" },
+      function (err, result) {
+        if (err) {
+          reject(console.log("error", err));
         } else {
-          resolve(bodyAsJson);
+          resolve(console.log("Vendor request is cached"));
         }
       }
     );
   });
-};
-const cacheVendorRequest = (vendorRequest) =>
-  new Promise((resolve, reject) => {
-    fs.writeFile("./Cache/VendorRequest.json", JSON.stringify(vendorRequest), { flag: "w" }, function (err, result) {
-      if (err) {
-        reject(console.log("error", err));
-      } else {
-        resolve(console.log("Vendor request is cached"));
-      }
-    });
-  });
 const vendorRequestCache = async () => {
   try {
-    const Request = await vendorRequest();
-    await cacheVendorRequest(Request);
+    const vendorRequestResponse = await axiosRequestInstance.get(
+      `https://www.bungie.net/Platform/Destiny2/${memberShipType}/Profile/${memberShipId}/Character/${characterId}/Vendors/672118013/?components=402`
+    );
+    await cacheVendorRequest(vendorRequestResponse.data);
   } catch (err) {
     console.log(err);
   }
