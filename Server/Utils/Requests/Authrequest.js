b874227e0edef9f1d14b54ca76b69fff5fff26c3
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-11-26 11:40:51 +0200

    Replaced $eval with evaluate method to prevent an selector error

--- Server/Utils/Requests/Authrequest.js
+++ Server/Utils/Requests/Authrequest.js
@@ -27,7 +27,10 @@ const authCodeRequest = async (client_id, username, password) => {
     await userNameElement.type(username, { delay: 100 });
     await passwordElement.type(password, { delay: 100 });
 
-    await page.$eval((selector) => document.querySelector(selector).click(), 'button[class="DjSvCZoKKfoNSmarsEcTS"]');
+    await page.evaluate(
+      (selector) => document.querySelector(selector).click(),
+      'button[class="DjSvCZoKKfoNSmarsEcTS"]'
+    );
     await page.waitForNavigation(),
       await page.evaluate((selector) => document.querySelector(selector).click(), 'input[id="imageLogin"]');
 
