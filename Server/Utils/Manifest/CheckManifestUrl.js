3e7988ca82a7cc0a51d35a656d45f446438d5e4a
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-11-26 12:33:16 +0200

    Added read config function and removed encoded string and axios instance

--- Server/Utils/Manifest/CheckManifestUrl.js
+++ Server/Utils/Manifest/CheckManifestUrl.js
@@ -4,17 +4,17 @@ const {
 } = config;
 const axios = require("axios");
 const fs = require("fs");
-const X = btoa(client_id + ":" + client_secret);
-const axiosRequestInstance = axios.create({
-  baseURL: "https://www.bungie.net/Platform/Destiny2/Manifest/",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "X-API-Key": ApiKey,
-    Authorization: "Basic " + X,
-    client_id: client_id,
-    code: code,
-  },
-});
+
+const readConfig = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("Server/Config/config.json", (error, result) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(result));
+    });
+  });
+};
 
 const saveManifestUrl = (config) => {
   return new Promise((resolve, reject) => {
