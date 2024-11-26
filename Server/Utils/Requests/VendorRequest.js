426ff5a486ff4b0662c6ded223b07c8e907c04c6
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-11-26 10:43:21 +0200

    get vendor refresh date

--- Server/Utils/Requests/VendorRequest.js
+++ Server/Utils/Requests/VendorRequest.js
@@ -26,10 +26,52 @@ const cacheVendorRequest = (vendorRequestResponse) =>
       }
     );
   });
-const vendorRequestCache = async () => {
+const getVendorRefreshDate = (vendorRequestResponse) => {
+  return new Promise((resolve, reject) => {
+    let vendorRefreshDate = null;
+    const vendorSales = vendorRequestResponse.Response.sales.data;
+    const vendorSalesKeyValues = Object.keys(vendorSales);
+    vendorSalesKeyValues.forEach((vendorSaleKey) => {
+      if (Object.hasOwn(vendorSales[vendorSaleKey], "overrideNextRefreshDate")) {
+        vendorRefreshDate = vendorSales[vendorSaleKey].overrideNextRefreshDate;
+        return;
+      }
+    });
+    if (!vendorRefreshDate) {
+      reject();
+    }
+    resolve(vendorRefreshDate);
+  });
+};
+
+const saveVendorRefreshDate = (config, vendorRefreshDate) =>
+  new Promise((resolve, reject) => {
+    const configWithVendorRefreshDate = Object.assign({}, config);
+    configWithVendorRefreshDate.Api.vendorRefreshDate = vendorRefreshDate;
+    fs.writeFile("Server/Config/config.json", JSON.stringify(configWithVendorRefreshDate), (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(console.log("Vendor Refresh date has been saved to config"));
+      }
+    });
+  });
+
+async function vendorRequest() {
+  const credentials = await readConfig();
+  const {
+    Api: { ApiKey, access_token },
+    SteamAccount: { memberShipType, memberShipId, characterId },
+  } = credentials;
   try {
-    const vendorRequestResponse = await axiosRequestInstance.get(
-      `https://www.bungie.net/Platform/Destiny2/${memberShipType}/Profile/${memberShipId}/Character/${characterId}/Vendors/672118013/?components=402`
+    const vendorRequestResponse = await axios.get(
+      `https://www.bungie.net/Platform/Destiny2/${memberShipType}/Profile/${memberShipId}/Character/${characterId}/Vendors/672118013/?components=402,310,305`,
+      {
+        headers: {
+          "X-API-KEY": ApiKey,
+          Authorization: "Bearer " + access_token,
+        },
+      }
     );
     await cacheVendorRequest(vendorRequestResponse.data);
   } catch (err) {
